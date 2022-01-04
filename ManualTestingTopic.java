64242de1bd5c6b45838afa34bc2c2e6f5fa72e88
Khyati Patel <Khyati@iMac.broadband>
2022-01-04 21:11:25 +0000

    this is my first commit

---
+++ ManualTestingTopic.java
@@ -0,0 +1,46 @@
+package com.manualtestingtopic;
+
+public class ManualTestingTopic {
+public static void main(String[] args) {
+
+    System.out.println("What is software?");
+    System.out.println("Software Risks");
+    System.out.println("What is Software Testing?");
+    System.out.println("Benefits of Software Testing?");
+    System.out.println("Who does Testing?");
+    System.out.println("Qualification of Software Tester");
+    System.out.println("What is Manual Testing?");
+    System.out.println("Software Development Life Cycle");
+    System.out.println("Software Testing Life Cycle");
+    System.out.println("Challenges of Manual Testing");
+    System.out.println("Principles of Software Testing");
+    System.out.println("Test Plan and it's benefits");
+    System.out.println("Test Scenario + Template");
+    System.out.println("Test Case + Template");
+    System.out.println("Test Scenario v/s Test Case");
+    System.out.println("Testing Techniques");
+    System.out.println("What is Bug?");
+    System.out.println("Bug Lifecycle");
+    System.out.println("Bug Template");
+    System.out.println("Severity V/s Priority");
+    System.out.println("Testing Levels");
+    System.out.println("Black Box Testing V/s White Box Testing");
+    System.out.println("Static V/s Dynamic Testing");
+    System.out.println("Exploratory Testing");
+    System.out.println("Ad-Hoc Testing");
+    System.out.println("Software Development Models");
+    System.out.println("Definition Of Ready/Done");
+    System.out.println("What Is TDD?");
+    System.out.println("What is BDD?");
+    System.out.println("Differance between TDD and BDD");
+    System.out.println("Maintenance");
+
+
+
+
+
+
+}
+
+
+}
